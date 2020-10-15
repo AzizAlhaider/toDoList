f17431cbd56601b9d6727a5fc0784853c13b45f0
AzizAlhaider <azizalhaider03@gmail.com>
2020-10-15 14:54:56 +0300

    1

---
+++ FinalProject/FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Aziz Alhaider on 10/15/20.
+//  Copyright © 2020 Aziz Alhaider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+struct list {
+    var title : String
+    var done : String
+    var importance : String
+    
+}
